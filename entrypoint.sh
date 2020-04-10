aa6cdfd3c6ef78dbffab0e7d0b6efb57d4b50252
Brandon <brandon@mac.local>
2020-04-10 16:08:03 -0500

    Fixed errors when pipeline didn't have annotations

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 set -e
 
+#Initialize build_number annotation variable
+annotation_variable='build_number'
+
 # Grab the current build information and output to json
 printf '\nBuild ID: %s' "$CF_BUILD_ID"
 
-if 
-    [ -z "$ANNOTATION_PREFIX" ]
+if [ "$ANNOTATION_NAME" = '${{ANNOTATION_NAME}}' ]
 then
     printf '\n\nANNOTATION_PREFIX is empty. Annotation name will be build_number'
 else
-    printf '\n\nANNOTATION_PREFIX=%s' "$ANNOTATION_PREFIX"
+    printf '\n\nAnnotation name passed in: %s' "$ANNOTATION_NAME"
+    annotation_variable="$ANNOTATION_NAME"
 fi
 
 printf '\n\nCurrent build JSON\n'
@@ -21,20 +24,14 @@ printf '\nJSON parsed pipeline-Id: %s \n' "$pipelineid"
 
 # If the annotation build_number already exists, increment it by 1
 # If the annotation build_number doesn't exist, initialize it at 1
-if 
-    printf '\nCurrent annotation build_number JSON\n'
-    codefresh get annotation pipeline $pipelineid build_number -o json 
-then 
-    build_number=$(codefresh get annotation pipeline $pipelineid build_number -o json | jq '.value' -j)    
-    printf '\nCurrent build_number: %s' "$build_number"
-    new_build_number=$((build_number+1))
-    
-    printf '\nBumped build_number: %s \n' "$new_build_number"
-    printf '\nCreating annotation: '
-    codefresh create annotation pipeline $pipelineid build_number=$new_build_number 
-else 
-    codefresh create annotation pipeline $pipelineid build_number=1 
-fi
-# Output the new value of the build_number annotation
-printf '\nUpdated annotation build_number JSON'
-codefresh get annotation pipeline $pipelineid build_number -o json
+printf '\nGet annotation JSON value using jq\n'
+build_number=$(codefresh get annotation pipeline $pipelineid $annotation_variable -o json | jq '.value' -j) || true
+printf '\nCurrent value: %s' "$build_number"
+new_build_number=$((build_number+1))
+
+printf '\nBumped value: %s \n' "$new_build_number"
+printf '\nCreating annotation: '
+codefresh create annotation pipeline $pipelineid $annotation_variable=$new_build_number
+
+printf '\nUpdated annotation build_number JSON\n'
+codefresh get annotation pipeline $pipelineid $annotation_variable -o json
